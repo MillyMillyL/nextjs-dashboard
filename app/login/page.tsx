36ab4af1acb0643f5527ea03a06bb002e2cd7e1e
Milly <liuhongmein@gmail.com>
2024-03-16 10:22:49 -0400

    finish dashboard

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,9 @@
-'use client';
+// 'use client';
 
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
+
+export const metadata: Metadata = { title: 'Login' };
 
 export default function LoginPage() {
   return (
